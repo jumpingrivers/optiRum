98787b6314637b29c03ccaa770b3690adc3f2e54
Steph Locke <stephanie.locke@optimumcredit.co.uk>
2014-01-14 15:38:06 +0000

    Initial style definitions

---
+++ Style/BuildExamples.R
@@ -0,0 +1,4 @@
+generatePDF(srcpath = file.path(getwd(),"Style"), srcname="ReportExample", 
+            destpath = file.path(getwd(),"Style"), destname="ReportExample", CLEANUP=TRUE)
+generatePDF(srcpath = file.path(getwd(),"Style"), srcname="PresentationExample", 
+            destpath = file.path(getwd(),"Style"), destname="PresentationExample", CLEANUP=TRUE)
