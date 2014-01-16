4962755e288433a4cbf69f6079473f293922138d
Steph Locke <stephanie.locke@optimumcredit.co.uk>
2014-01-16 14:56:16 +0000

    added sanitisation function for use with xtable

---
+++ tests/testthat/test-sanitise.R
@@ -0,0 +1,7 @@
+context("sanitise")
+
+test_that("sanitise correctly sanitises string",{
+          expect_true(sanitise("$><|{}%&_#^~\\")=="\\$$>$$<$$|$\\{\\}\\%\\&\\_\\#\\verb|^|\\~{}$\\backslash$") #tests existing stuff
+          expect_true(sanitise("[]")=="{}[]") #tests basic functionality we added
+          expect_true(sanitise("{}[]")=="\\{\\}{}[]") #tests the basic functionality added doesn't impact existing functionality
+          })
