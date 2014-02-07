010c1f6960be01fe2d720004b9f02ebc44e0ed4c
Steph Locke <stephanie.locke@optimumcredit.co.uk>
2014-02-07 11:58:03 +0000

    Moved into optINTERNAL

--- Style/BuildExamples.R
+++
@@ -1,5 +0,0 @@
-library("optiRum")
-generatePDF(srcpath = file.path(getwd(),"Style"), srcname="ReportExample", 
-            destpath = file.path(getwd(),"Style"), destname="ReportExample", CLEANUP=TRUE)
-generatePDF(srcpath = file.path(getwd(),"Style"), srcname="PresentationExample", 
-            destpath = file.path(getwd(),"Style"), destname="PresentationExample", CLEANUP=TRUE)
--- tests/testthat/test-optiplum.R
+++
@@ -1,6 +0,0 @@
-context("optiplum")
-
-test_that("optiplum - produces the correct hex code",{
- optiplum()
-  expect_true(optiplum=="#813D72")
-})
