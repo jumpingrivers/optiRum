283b6dc8247a4dd61d2eecee0c6cf36998bd58f0
Steph Locke <stephanie.locke@optimumcredit.net>
2015-04-21 09:51:07 +0100

    Test coverage - *_format

--- tests/testthat/test-thousands.R
+++ tests/testthat/test-thousands.R
@@ -5,3 +5,9 @@ test_that("thousands format always adds k", {
     expect_equal(thousands(1e+06), "1,000k")
     expect_equal(thousands(1e+09), "1,000,000k")
 })
+
+test_that("thousands_format format always adds k", {
+  expect_equal(thousands_format(1000), "1k")
+  expect_equal(thousands_format(1e+06), "1,000k")
+  expect_equal(thousands_format(1e+09), "1,000,000k")
+})
