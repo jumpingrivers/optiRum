36115066775510d5488c8095a29769f1d683245c
Stephanie Locke <stephanie.locke@optimumcredit.co.uk>
2015-04-22 19:26:00 +0100

    Corrected unit tests I forgot how to use my own helper functions, d'oh!

--- tests/testthat/test-thousands.R
+++ tests/testthat/test-thousands.R
@@ -7,7 +7,7 @@ test_that("thousands format always adds k", {
 })
 
 test_that("thousands_format format always adds k", {
-  expect_equal(thousands_format(1000), "1k")
-  expect_equal(thousands_format(1e+06), "1,000k")
-  expect_equal(thousands_format(1e+09), "1,000,000k")
+  expect_equal(thousands_format()(1000), "1k")
+  expect_equal(thousands_format()(1e+06), "1,000k")
+  expect_equal(thousands_format()(1e+09), "1,000,000k")
 })
