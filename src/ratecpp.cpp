6c75a0c8f85493113db450f82d1e5c09e68e1d27
ritagior <rgiordano@gmx.com>
2018-07-12 16:17:03 +0200

    minor change on power

--- src/ratecpp.cpp
+++ src/ratecpp.cpp
@@ -8,30 +8,61 @@ using namespace Rcpp;
 //' @param pv Present value i.e. loan advance (should be positive)
 //' @export
 // [[Rcpp::export]]
-NumericVector ratecpp(int nper,int pmt, int pv) {
-  double rate1 = 0.01;
-  double rate2 = 0.005;
-  NumericVector newrate = 0;
-  int n = 10;
+NumericVector ratecpp(const NumericVector nper, const NumericVector pmt, const NumericVector pv) 
+{
+  // if (isnumber(nper) && isnumber(pmt))
+  // {
+  //   printf ("Error opening file");
+  // }
   
-  for(int i = 0; i < n; i++) {
-    double pv1 = -pmt/rate1*(1 - 1/(pow(1 + rate1, nper)))  - pv;
-    double pv2 = -pmt/rate2*(1 - 1/(pow(1 + rate2, nper)))  - pv;
+  // else 
+  // {
+    pow(pmt, nper);
+    double rate1 = 0.01;
+    double rate2 = 0.005;
+    NumericVector x = 1 + rate1;
+    NumericVector y = 1 + rate2;
+    NumericVector newrate = 0;
+    int n = 10;
+    
+    //NumericVector power1(nper.size());
+    //std::transform((1+rate1), nper.begin(), nper.size(), ::pow);
+    for(int i = 0; i < n; i++) {
       
-    if (pv1 != pv2) {
+    NumericVector pv1 = -pmt/rate1 * (1 - 1/(pow(x, nper))) - pv;
+    NumericVector pv2 = -pmt/rate2 * (1 - 1/(pow(y, nper))) - pv;
+    
+      // Convert the elemnt of the double vector to a double
+     //  double pv1_0 = pv1[0];
+     //  Rcpp::Rcout << pv1_0;
+     // //Rf_PrintValue(pv1_0);
+     //  //double pv1_1 = pv1[1];
+     //  double pv2_0 = pv2[0];
+     //  Rcpp::Rcout << pv2_0;
+     //  //double pv2_1 = pv2[1];
+     
+     LogicalVector pv12 = pv1 != pv2;
+      if (pv12) {
+        //double newrate = (pv1 * rate2 - pv2 * rate1)/(pv1 - pv2);
+      //NumericVector newrate = NumericVector::create((pv1 * rate2 - pv2 * rate1)/(pv1 - pv2));
       NumericVector newrate = (pv1 * rate2 - pv2 * rate1)/(pv1 - pv2);
-    }
+      std::cout <<  newrate << std::endl;
+      return newrate;
+      }
       
-    if (std::abs(pv1) > std::abs(pv2)) {
-      NumericVector rate1 = newrate;
-    } 
-    else {
-      NumericVector rate2 = newrate;
+      // if (abs(pv1) > abs(pv2)) {
+      //   NumericVector rate1 = newrate;
+      //   return rate1;
+      // } else {
+      //   NumericVector rate2 = newrate;
+      //   return rate2;
+      //}
     }
-  }
-  return rate1;
+    //return rate1;
+    return newrate;
+  // }
+  //return ratecpp(nper, pmt, pv);
 }
-  //return ratecpp(nper, pmt, pv, fv);
 
 
 
@@ -42,5 +73,5 @@ NumericVector ratecpp(int nper,int pmt, int pv) {
 
 /*** R
 df<-data.frame(nper=c(12,12),pmt=c(-500,-400),pv=c(3000,3000))
-ratecpp(df$nper, df$pmt, df$pv)
+ratecpp(df$nper,df$pmt,df$pv)
 */
